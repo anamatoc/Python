6f22adc686e770b1b29d62beba07785cc797f08a
anamatoc <132319304+anamatoc@users.noreply.github.com>
2023-05-18 23:35:44 -0400

    Add files via upload
    Budget

--- main.py
+++ main.py
@@ -2,77 +2,68 @@ import os
 import csv
 
 #Set path for files
-election_csv = os.path.join("..", "Resources", "election_data.csv")
-output = os.path.join("..", "Pypoll", "elections.txt")
+budget_csv = os.path.join("..", "Resources", "budget_data.csv")
+output = os.path.join("..", "Analysis", "budget.txt")
+
 
 #Variables
-total_votes = 0
-count_candidates = []
-candidate_a = 0
-candidate_b = 0
-candidate_c = 0
-single_candidates = []
-
-with open(election_csv) as csvfile:
-    election_vote = csv.reader(csvfile, delimiter=',')
-    header = next(election_vote)
-    
-    for row in election_vote:
-        column_data = row[2]
-        total_votes +=1
-
-        #Now find the single candidates 
-        if column_data not in single_candidates:
-            single_candidates.append(column_data)
-
-        #Porcentage for each candidate (won votes)
-        if column_data == single_candidates[0]:
-            candidate_a +=1
-
-        elif column_data == single_candidates[1]:
-            candidate_b +=1
-        
-        elif column_data == single_candidates[2]:
-            candidate_c +=1
-
-        # Finding the winner truogh graders
-        if candidate_a > candidate_b and candidate_c:
-            winner = single_candidates[0]
-        
-        elif candidate_b > candidate_a and candidate_c:
-            winner = single_candidates[1]
-
-        elif candidate_c > candidate_a and candidate_b:
-            winner = single_candidates[2]
-        
-#Finding porcentage for each candidate
-percentage_candidate_a = (candidate_a / total_votes) * 100
-percentage_candidate_b = (candidate_b / total_votes) * 100
-percentage_candidate_c = (candidate_c / total_votes) * 100
+total_months = []
+total_profits = []
+increase_profit = ["", 0]
+decrease_profit = ["", 99999999999]
+month_count = 0
+net_profit = 0
 
 
+#open file
+with open(budget_csv, "r") as csvfile:
+    csvfile = csv.reader(csvfile)
+    header = next(csvfile)
+    first_row = next(csvfile)
+
+    month_count+=1
+    net_profit+=int(first_row[1])
+    previous_net=int(first_row[1])
+
+
+#Dates and profits
+
+    for row in csvfile:
+        month_count+=1
+        net_profit+=int(row[1])
+
+
+        net_change=int(row[1])-previous_net
+        previous_net=int(row[1])
+
+
+        total_months.append(row[0])
+        total_profits.append(net_change)
+
+        if net_change > increase_profit[1]:
+            increase_profit[1]=net_change
+            increase_profit[0]=row[0]
+
+        if net_change < decrease_profit[1]:
+            decrease_profit[1]=net_change
+            decrease_profit[0]=row[0]
+            
+
+monthly_average=sum(total_profits)/len(total_profits)
 
 text=f"""
-Election Results
+Financial Analysis
 ----------------------------
-
-Total Votes: {total_votes}
-
-----------------------------
-
-{single_candidates[0]}: {percentage_candidate_a:.3f}% ({candidate_a})
-{single_candidates[1]}: {percentage_candidate_b:.3f}% ({candidate_b})
-{single_candidates[2]}: {percentage_candidate_c:.3f}% ({candidate_c})
-
-----------------------------
-
-Winner: {winner}
-
-----------------------------
+Total Months: {month_count}
+Total: ${net_profit}
+Average Change: ${monthly_average:.2f}
+Greatest Increase in Profits: {increase_profit[0]} (${increase_profit[1]})
+Greatest Decrease in Profits: {decrease_profit[0]} (${decrease_profit[1]})
 
 """
 print(text)
-
-# Write results to the output file
-with open(output , 'w') as file:
+with open(output, "w") as file:
     file.write(text)
+
+    
+
